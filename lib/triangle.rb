37928321dabd1e97f3b90177a89764afab418c30
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-01 00:49:26 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,5 +7,23 @@ class Triangle
     @side_two = side_two
     @side_three = side_three
   end
-  
+
+  def kind
+    
+  end
+
+end
+
+class TriangleError < StandardError
+  if self.side_one + self.side_two + self.side_three != 180 then
+
+  else
+  if self.side_two == self.side_three == self.side_one == 60
+    then :equilateral
+  elsif self.side_one == self.side_two || self.side_one == self.side_three || self.side_two == self.side_three
+    then :isosceles
+  else :scalene
+  end
+end
+    
 end
